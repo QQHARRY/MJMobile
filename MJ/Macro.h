3950e7596626732e053512b72daae715f1b1d38c
Alur <heroalur@gmail.com>
2014-11-28 15:41:29 +0800

    macro api

--- MJ/Macro.h
+++ MJ/Macro.h
@@ -5,5 +5,27 @@
 #define SERVER_URL @"http://115.28.76.58:9090/kwoa/device/"
 
 // API
+// 1.申请绑定设备
+#define API_REG @"regdevice"
+// 2.登录
 #define API_LOGIN @"applogin"
+// 4.获取公告列表
+#define API_ANNC_LIST @"getpublicannclist"
+// 6.获取签呈列表
+#define API_PETITION_LIST @"getpetitionlist"
+// 7.获取签呈详情
+#define API_PETITION_DETAIL @"getpetitiondetails"
+// 10.签呈审批
+#define API_PETITION_APPROVE @"petitionapproving"
+// 11.获取未读跟进提醒数量
+#define API_ALERT_COUNT @"getalertcount"
+// 12.获取跟进提醒列表
+#define API_ALERT_LIST @"getalertlist"
+// 13.标记跟进提醒
+#define API_ALERT_FINISH @"alertfinished"
+
+//#define API_LOGIN @"applogin"
+//#define API_LOGIN @"applogin"
+//#define API_LOGIN @"applogin"
+//#define API_LOGIN @"applogin"
 
