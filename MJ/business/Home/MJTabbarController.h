1c815fafe69789ec741c9f9fae47a38dd740b0c2
gentingharry <258563260@qq.com>
2015-02-14 17:13:06 +0800

    fix bug

---
+++ MJ/business/Home/MJTabbarController.h
@@ -0,0 +1,13 @@
+//
+//  MJTabbarController.h
+//  MJ
+//
+//  Created by harry on 15/2/14.
+//  Copyright (c) 2015年 Simtoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MJTabbarController : UITabBarController<UITabBarControllerDelegate>
+
+@end
