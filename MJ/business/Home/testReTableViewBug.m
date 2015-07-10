1a83cfe6d66fe98846e814d11f5ed1666956d8d0
gentingharry <258563260@qq.com>
2015-07-10 10:19:49 +0800

    12312

--- MJ/business/Home/testReTableViewBug.m
+++ MJ/business/Home/testReTableViewBug.m
@@ -27,10 +27,42 @@
     self.manager = [[RETableViewManager alloc] initWithTableView:self.tableView];
     self.section = [[RETableViewSection alloc] initWithHeaderTitle:@"房源信息"];
     [self.manager addSection:self.section];
-    for (int i =0; i < 50; i++) {
-        
-        RETextItem*item = [[RETextItem alloc]  initWithTitle:[NSString stringWithFormat:@"%d",i]];
-        [self.section addItem:item];
+    for (int i = 1; i < 50; i++) {
+        RETableViewItem*item = nil;
+        switch (i%4)
+        {
+            case 0:
+            {
+                item = [[RETextItem alloc]  initWithTitle:[NSString stringWithFormat:@"Text%d",i]];
+                [self.section addItem:item];
+            }
+                break;
+            case 1:
+            {
+                item = [[RERadioItem alloc]  initWithTitle:[NSString stringWithFormat:@"Radio%d",i]];
+                [self.section addItem:item];
+            }
+                break;
+            case 2:
+            {
+                item = [[ReMultiTextItem alloc]  initWithTitle:[NSString stringWithFormat:@"MultiText%d",i]];
+                [self.section addItem:item];
+            }
+                break;
+            case 3:
+            {
+                item = [[RENumberItem alloc]  initWithTitle:[NSString stringWithFormat:@"Number%d",i]];
+                [self.section addItem:item];
+            }
+                break;
+                
+            default:
+                break;
+        }
+        if (i%5 == 0)
+        {
+            item.enabled = NO;
+        }
     }
 }
 
